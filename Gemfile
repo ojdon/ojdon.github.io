167c675ca0773b3f7f3cefd05ee955cc22fbf28b
Ollie Reardon <ojreardon@gmail.com>
2023-07-16 22:42:16 +0100

    Initial tailwind install

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
 gem 'github-pages'
 gem 'jekyll-watch'
-gem "webrick"
+gem 'webrick'
+group :jekyll_plugins do
+  gem 'jekyll-postcss'
+end
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  purge: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+  ],
+  darkMode: false,
   theme: {
     extend: {},
   },
-  plugins: [],
+  variants: {},
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
