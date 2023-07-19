8c559468d041801fe8e3b85f10bc46476ce30ffb
Ollie Reardon <ojreardon@gmail.com>
2023-07-19 22:58:07 +0100

    New homepage background

--- postcss.config.js
+++ postcss.config.js
@@ -2,19 +2,10 @@ const jekyllEnv = process.env.JEKYLL_ENV || "development";
 
 module.exports = {
     plugins: [
-        require("postcss-import"),
-        require("tailwindcss")("./tailwind.config.js"),
-        require("autoprefixer"),
-        ...(jekyllEnv == "development"
-            ? [
-                require("@fullhuman/postcss-purgecss")({
-                    content: ["!(_site|node_modules)/**/*.+(html|js|md)", "*.html"],
-                    whitelistPatternsChildren: [/highlight/],
-                    defaultExtractor: (content) =>
-                        content.match(/[\w-/:]+(?<!:)/g) || [],
-                }),
-                require("cssnano")({ preset: "default" }),
-            ]
+        require('tailwindcss'),
+        require('autoprefixer'),
+        ...(process.env.JEKYLL_ENV === 'production'
+            ? [require('cssnano')({ preset: 'default' })]
             : [])
     ]
-};
+}
